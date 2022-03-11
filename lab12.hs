08a8b1bc780f6c486e0677f0c9446af5c4520a81
James H. Schader <schadejh@dukes.jmu.edu>
2022-03-10 21:28:12 -0500

    Update lab12.hs

--- lab12.hs
+++ lab12.hs
@@ -28,7 +28,6 @@ quicksort ns
   | length ns > 1 = quicksort lower ++ [(head ns)] ++ quicksort upper
       where upper = filter (\x -> x > (head ns)) (tail ns)
             lower = filter (\x -> x <= (head ns)) (tail ns)
-
   | otherwise = []
 
 -- pairToList :: Pair a -> [a]
