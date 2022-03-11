88232063edecbe251daa2ad696fd53b1589771ca
James H. Schader <schadejh@dukes.jmu.edu>
2022-03-10 23:10:18 -0500

    Update lab12.hs

--- lab12.hs
+++ lab12.hs
@@ -52,8 +52,8 @@ data Movement
 
 mirror :: (Movement, Int) -> (Movement, Int)
 mirror (m,i) = case m of
-  North -> (South, i)
-  South -> (North, i)
-  East -> (West, i)
-  West -> (East, i)
+  North, i -> (South, i)
+  South, i -> (North, i)
+  East, i -> (West, i)
+  West, i -> (East, i)
 
